ebc38e972971a4a21a995b4de93df8f8adc75e23
VimbaiChikoore <158522823+VimbaiChikoore@users.noreply.github.com>
2024-04-10 14:07:28 -0400

    updattes

--- public/js/main.js
+++ public/js/main.js
@@ -2,15 +2,13 @@
 
 async function update_dashboard(){
   try {
+    // Ticket 1 Unresolved
     const card1_holder = document.getElementById('ticket1_id');
+    // Ticket 2 - Overdue
     const card2_holder = document.getElementById('ticket2_id');
 
     const get_data = await fetch("data/content.json");
     const store_data = await get_data.json();
-
-    // Clear existing content
-    card1_holder.innerHTML = '';
-    card2_holder.innerHTML = '';
 
     store_data.tickets.forEach(ticket_item=> {
 
@@ -20,6 +18,7 @@ async function update_dashboard(){
                                 <p class="card-text ticket-count">${ticket_item.ticket1_result}</p>`;
       card1_holder.append(ticket1_data);
 
+      // Creating HTML elements for ticket 2- Overdue
       const ticket2_data = document.createElement('div');
       ticket2_data.innerHTML = `<h5 class="card-title ticket-name">${ticket_item.ticket2_name}</h5>
                               <p class="card-text ticket-count">${ticket_item.ticket2_result}</p>`;
