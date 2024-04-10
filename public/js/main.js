aa4f208b89f8fdeda71cd3303508cc7cd132a7c4
VimbaiChikoore <158522823+VimbaiChikoore@users.noreply.github.com>
2024-04-10 14:22:58 -0400

    card 1 done
    Doubling bug continues

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,15 @@
 // Async/await method
-
+// V: Prof i'm sorry i couldn't solve this quadrupling bug
 async function update_dashboard(){
   try {
     // Ticket 1 Unresolved
     const card1_holder = document.getElementById('ticket1_id');
     // Ticket 2 - Overdue
     const card2_holder = document.getElementById('ticket2_id');
+    // Ticket 3 - Open
+    const card3_holder = document.getElementById('ticket3_id');
+    // Ticket 4 - Open
+    const card4_holder = document.getElementById('ticket4_id');
 
     const get_data = await fetch("data/content.json");
     const store_data = await get_data.json();
@@ -24,9 +28,21 @@ async function update_dashboard(){
                               <p class="card-text ticket-count">${ticket_item.ticket2_result}</p>`;
       card2_holder.append(ticket2_data);
 
+      // Creating HTML elements for ticket 3- Open
+      const ticket3_data = document.createElement('div');
+      ticket3_data.innerHTML = `<h5 class="card-title ticket-name">${ticket_item.ticket3_name}</h5>
+                                <p class="card-text ticket-count">${ticket_item.ticket3_result}</p>`;
+      card3_holder.append(ticket3_data);
+
+      // Creating HTML elements for ticket 4- Open
+      const ticket4_data = document.createElement('div');
+      ticket4_data.innerHTML = `<h5 class="card-title ticket-name">${ticket_item.ticket4_name}</h5>
+                                      <p class="card-text ticket-count">${ticket_item.ticket4_result}</p>`;
+      card4_holder.append(ticket4_data);
+
     });
   } catch (error) {
-    console.log('error:', error);
+    console.log('error: something is wrong', error);
   }
 }
 
